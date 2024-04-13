4b7ddeaf336ad5156eb6a7057a59765b0c20338d
madeleoh <maddieeleonore2009@gmail.com>
2024-04-13 11:37:29 -0400

    make it work

--- Assets/Scripts/TurretLook.cs
+++ Assets/Scripts/TurretLook.cs
@@ -19,6 +19,7 @@ public class TurretLook : MonoBehaviour
         if (laserSwitch.lasersAreOff)
         {
             laser.SetActive(false);
+            return;
         }
         transform.LookAt(player);
     }
